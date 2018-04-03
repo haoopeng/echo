52d585473c603c783e067714ec7b963e7be7f17a
Hao Peng <penghao@umail.iu.edu>
2018-04-03 11:42:46 -0400

    update js

--- js/main.js
+++ js/main.js
@@ -434,3 +434,21 @@ function unorderedPairs(s) { // returns the list of all unordered pairs from s
 function roundToTwo(num) {
     return +(Math.round(num + "e+2")  + "e-2");
 }
+
+// Infor function
+function closeInfoArea() {
+    $("#info")
+      .css("right", "0px")
+      .animate({"right": "-100%"}, 800)
+    $("nav").show()
+  }
+
+function showInfoArea() {
+  $("#info")
+    .css("right", "-100%")
+    .animate({"right": "0px"}, 800)
+  $("nav").hide()
+}
+
+$("#info-tab").on("click", showInfoArea);
+$("#info-close").on("click", closeInfoArea);
